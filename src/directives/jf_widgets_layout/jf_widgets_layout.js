ef90a15f17a795e14277d2494f305fd2bda64235
dannyr <dannyr@jfrog.com>
2016-10-13 20:39:15 +0300

    jf-widgets-layout

--- src/directives/jf_widgets_layout/jf_widgets_layout.js
+++ src/directives/jf_widgets_layout/jf_widgets_layout.js
@@ -8,17 +8,27 @@ class jfWidgetsLayoutController {
         this.$compile = $compile;
         this.$templateRequest = $templateRequest;
 
+        this.setDefaultOptions();
+
         let onChange = (newval,oldval) => {
             if (this.templatesLoadStarted && !this.templatesLoaded) return;
             this.updateCss();
             this.loadTemplates().then(()=>{
-                $timeout(()=>this.compileDirectives());
+                $timeout(()=>this.compileElements());
             })
         };
+
         $scope.$watch('jfWidgetsLayout.layout', onChange);
 //        $scope.$watch('jfWidgetsLayout.options', onChange, true);
     }
 
+    setDefaultOptions() {
+        if (!this.options) this.options = {}
+        if (!this.options.padding) this.options.padding = 10;
+        if (!this.options.minHeight) this.options.minHeight = 'initial';
+        if (!this.options.backColor) this.options.backColor = 'transparent';
+        if (this.options.outerPadding === undefined) this.options.outerPadding = true;
+    }
 
     loadTemplates() {
         let defer = this.$q.defer();
@@ -31,58 +41,74 @@ class jfWidgetsLayoutController {
         this.templatesLoadStarted = true;
         let fired = 0, completed = 0;
         this.layout.forEach((row) => {
-            row.forEach((widget) => {
-                if (widget.templateUrl && !widget.template) {
-                    fired++;
-                    this.$templateRequest(widget.templateUrl)
-                        .then((template) => {
-                            widget.template = this.$sce.trustAsHtml(template);
-                        })
-                        .finally(()=>{
-                            completed++;
+            row.forEach((layoutDef) => {
+                let widget = this.widgets[layoutDef.widget];
+                if (widget) {
+                    if (widget.templateUrl && !widget.template) {
+                        fired++;
+                        this.$templateRequest(widget.templateUrl)
+                            .then((template) => {
+                                widget.template = this.$sce.trustAsHtml(template);
+                            })
+                            .finally(()=>{
+                                completed++;
 //                            console.log('completed ' + completed + ' out of ' + fired);
-                            if (fired === completed) {
-                                this.templatesLoaded = true;
-                                defer.resolve();
-                            }
-                        })
+                                if (fired === completed) {
+                                    this.templatesLoaded = true;
+                                    defer.resolve();
+                                }
+                            })
+                    }
+                    else if (widget.template) widget.template = this.$sce.trustAsHtml(widget.template)
                 }
-                else if (widget.template) widget.template = this.$sce.trustAsHtml(widget.template)
             });
         });
 
         if (!fired) defer.resolve();
         return defer.promise;
     }
+
     updateCss() {
         this.cssRules = {};
         let currentX, currentY = 0;
+        let emptyWidgetNextIndex = 0;
         this.layout.forEach((row) => {
             currentX = 0;
             let topHeight = 0;
-            row.forEach((widget) => {
-                this.cssRules[widget.id] = {
+            row.forEach((layoutDef) => {
+                let widget = this.widgets[layoutDef.widget];
+                if (!widget) {
+                    layoutDef.widget = 'widget'+emptyWidgetNextIndex;
+                    emptyWidgetNextIndex++;
+                }
+                this.cssRules[layoutDef.widget] = {
                     top: currentY + '%',
                     left: currentX + '%',
-                    bottom: (100 - (currentY + widget.percentHeight)) + '%',
-                    right: (100 - (currentX + widget.percentWidth)) + '%',
+                    bottom: (100 - (currentY + layoutDef.percentHeight)) + '%',
+                    right: (100 - (currentX + layoutDef.percentWidth)) + '%',
                     padding: (this.options.padding/2) + 'px'
                 }
-                currentX += widget.percentWidth;
-                if (widget.percentHeight > topHeight) topHeight = widget.percentHeight;
+                currentX += layoutDef.percentWidth;
+                if (layoutDef.percentHeight > topHeight) topHeight = layoutDef.percentHeight;
             })
             currentY += topHeight;
         })
 
+        let pad = this.options.outerPadding ? this.options.padding/2 : -this.options.padding/2;
         this.padderCss = {
-            top: (this.options.padding/2) + 'px',
-            left: (this.options.padding/2) + 'px',
-            bottom: (this.options.padding/2) + 'px',
-            right: (this.options.padding/2) + 'px'
+            top: pad + 'px',
+            left: pad + 'px',
+            bottom: pad + 'px',
+            right: pad + 'px'
+        }
+
+        this.containerCss = {
+            'min-height': this.options.minHeight + 'px',
+            'background-color': this.options.backColor
         }
     }
 
-    compileDirectives() {
+    compileElements() {
         let elems = $('.compile-children');
         for (let i = 0; i< elems.length; i++) {
             let elem = $(elems[i]);
@@ -96,6 +122,7 @@ class jfWidgetsLayoutController {
             if (widget.controller) {
                 widget.controller.prototype.$scope = scope;
                 widget.controller.prototype.$widgetObject = widget;
+                widget.controller.prototype.$layoutObject = this._getLayoutByWidget(elem.prop('id'));
                 widget.controller.prototype.$widgetLayoutManager = this;
 
                 let controllerInstance = this.$injector.instantiate(widget.controller);
@@ -121,15 +148,19 @@ class jfWidgetsLayoutController {
         }
     }
     _getWidgetById(id) {
-        let widget;
+        let widget = this.widgets[id];
+        return widget;
+    }
+    _getLayoutByWidget(id) {
+        let layout;
 
         for (let i in this.layout) {
             let row = this.layout[i];
-            widget = _.find(row, {id: id});
-            if (widget) break;
+            layout = _.find(row, {widget: id});
+            if (layout) break;
         }
 
-        return widget;
+        return layout;
     }
 }
 
@@ -139,6 +170,7 @@ export function jfWidgetsLayout() {
         controllerAs: 'jfWidgetsLayout',
         bindToController: true,
         scope: {
+            widgets: '=',
             layout: '=',
             options: '='
         },
