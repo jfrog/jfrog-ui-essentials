d6833b70d8e7b4145585b93497118097be773f74
dannyr <dannyr@jfrog.com>
2016-11-27 20:45:42 +0200

    Almost done hybriding for b.t.poc

---
+++ dist/ts/bower.imports.ts
@@ -0,0 +1,45 @@
+import '../../bower_components/angular/angular.min.js';
+import '../../bower_components/angular-animate/angular-animate.min.js';
+import '../../bower_components/angular-mocks/angular-mocks.js';
+import '../../bower_components/angular-cookies/angular-cookies.js';
+import '../../bower_components/angular-sanitize/angular-sanitize.js';
+import '../../bower_components/angular-messages/angular-messages.js';
+import '../../bower_components/angular-route/angular-route.min.js';
+import '../../bower_components/angular-ui-router/release/angular-ui-router.js';
+import '../../bower_components/angular-bootstrap/ui-bootstrap-tpls.js';
+import '../../bower_components/angular-ui-grid/ui-grid.js';
+import '../../bower_components/angular-resource/angular-resource.js';
+import '../../bower_components/angularjs-toaster/toaster.js';
+import '../../bower_components/angular-ui-utils/ui-utils.js';
+import '../../bower_components/angular-ui-select/dist/select.js';
+
+
+import '../../bower_components/ng-clip/src/ngClip.js';
+
+
+(window as any).CodeMirror = require('../../bower_components/codemirror/lib/codemirror.js');
+import '../../bower_components/angular-ui-codemirror/ui-codemirror.js';
+import '../../bower_components/codemirror/addon/mode/overlay.js';
+import '../../bower_components/codemirror/mode/xml/xml.js';
+import '../../bower_components/codemirror/mode/javascript/javascript.js';
+import '../../bower_components/codemirror/mode/clike/clike.js';
+import '../../bower_components/codemirror/addon/dialog/dialog.js';
+import '../../bower_components/codemirror/addon/search/search.js';
+import '../../bower_components/codemirror/addon/search/searchcursor.js';
+
+
+
+var $ = require("jquery");
+(window as any).$ = (window as any).jQuery = (window as any).jquery = $;
+import "../../bower_components/jQuery-contextMenu/dist/jquery.contextMenu.min.js";
+
+import '../../bower_components/jquery-ui/jquery-ui.js';
+import '../../bower_components/lodash/lodash.js';
+import '../../bower_components/tooltipster/js/jquery.tooltipster.min.js';
+
+(window as any).ZeroClipboard = require('../../bower_components/zeroclipboard/dist/ZeroClipboard.js');
+(window as any).Clipboard = require('../../bower_components/clipboard/dist/clipboard.min.js');
+
+import '../../bower_components/jfrog-ui-essentials/dist/vendorScripts';
+import '../../bower_components/jfrog-ui-essentials/dist/jfrog-ui-essentials';
+
---
+++ dist/ts/jfrog-ui-essentials-ng2.ts
@@ -0,0 +1,308 @@
+import "./bower.imports";
+import {NgModule, Component} from "@angular/core";
+import {UpgradeAdapter} from "@angular/upgrade";
+import {TestComponent} from "./components/test-component/test.component";
+
+const NG1_MODULE = 'ng1module'
+let angular = (window as any).angular;
+let _ = (window as any)._;
+let $ = (window as any).$;
+
+export class JFrogUIEssentials {
+    private upgradeAdapter:UpgradeAdapter;
+    private wrappers = [];
+
+    public constructor(_upgradeAdapter:any) {
+        this.upgradeAdapter = _upgradeAdapter;
+
+        angular.module(NG1_MODULE,['jfrog.ui.essentials']);
+    }
+
+    public downgradeRootComponent(rootComponent:Component, selector?:string) {
+        angular.module(NG1_MODULE).directive(selector,this.upgradeAdapter.downgradeNg2Component(rootComponent as any));
+    }
+
+    public getJFrogUIModule() {  
+
+        this.createWrappers();
+
+        this.createNg1FormWrapper();
+
+        //Upgrade services
+        this.upgradeAdapter.upgradeNg1Provider('JFrogUILibConfig');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogGridFactory');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogNotifications');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogEventBus');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogModal');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogUIUtils');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogDownload');
+        this.upgradeAdapter.upgradeNg1Provider('JFrogIFrameDownload');
+
+        let wrappers = this.wrappers;
+
+        let components = [
+            TestComponent,
+//            this.upgradeAdapter.upgradeNg1Component('jfCheckbox')
+        ];
+        @NgModule({
+            declarations: wrappers.concat(components),
+            exports: wrappers.concat(components)
+        })
+        class JFrogUIModule {
+        }
+
+        return JFrogUIModule
+    }
+
+    public bootstrap() {
+        angular.element(document).ready(() => {
+            this.upgradeAdapter.bootstrap(document.documentElement, [NG1_MODULE]);
+        })
+    }
+
+    private createNg1FormWrapper() {
+
+        angular.module(NG1_MODULE).component('jfrogNg1Form',{
+            bindings: {
+                ng1Template: '@',
+                ng2Controller: '='
+            },
+            template: `<ng-include src="$ctrl.ng1Template"></ng-include>`
+        })
+
+        let wrapper = this.upgradeAdapter.upgradeNg1Component('jfrogNg1Form');
+
+        this.wrappers.push(wrapper);
+
+    }
+
+    createWrappers() {
+        this.createWrapper('jf-list-maker','jfrog-list-maker',{
+            values: '=',
+            label: '@',
+            helpTooltip: '=',
+            objectName: '@',
+            ngDisabled: '=',
+            noSort: '=?',
+            minLength: '@',
+            inputType: '@?',
+            predefinedValues: '=?',
+            placeholder: '@?',
+            listId: '@'
+        });
+
+        this.createWrapper('jf-multi-dropdown','jfrog-multi-dropdown',{
+            title: '@',
+            filterPlaceholder: '@',
+            noItemsMessage: '@',
+            items: '=',
+            onChange: '&?',
+            dropdownOpened: '='
+        });
+
+        this.createWrapper('jf-panel','jfrog-panel',{
+            jfPanelHeading: '@',
+            jfPanelHelpTooltip: '@',
+            jfPanelClasses: '@',
+        },true);
+
+        this.createWrapper('jf-drag-drop','jfrog-drag-drop', {
+            includeList: '=',
+            excludeList: '=',
+            includeDisplayField: '@',
+            excludeDisplayField: '@',
+            includeListId: '@',
+            excludeListId: '@',
+            objectsName: '@',
+            headers: '=',
+            onChange: '&',
+            customTemplate: '=?',
+            customTemplateScope: '=?',
+            usePagination: '=?',
+            commObject: '=',
+            ngDisabled: '='
+        });
+
+        this.createWrapper('jf-field','jfrog-field',{
+            animated: '@',
+            validations: '@',
+            autofocus: '=',
+            invalidateOnSubmit: '@',
+            alwaysShowErrors: '@',
+            dontValidateDisabled: '@',
+            delayedInit: '=',
+            dontPushDownErrors: '='
+        },true);
+
+        this.createWrapper('jf-checkbox','jfrog-checkbox',{
+            text: '@?'
+        },true);
+
+        this.createWrapper('jf-help-tooltip','jfrog-help-tooltip',{
+            placement: '@?',
+            text: '@?',
+            html: '=',
+            maxWidth: '@'
+        });
+
+        this.createWrapper('jf-grid','jfrog-grid',{
+            gridOptions: '=',
+            filterField: '@?',
+            filterField2: '@?',
+            filterOnChange: '@?',
+            autoFocus: '@',
+            objectName: '@'
+        });
+
+        this.createWrapper('jf-ui-select','jfrog-ui-select',{
+            jfSelectModel: '=',
+            jfSelectOptions: '=',
+            jfSelectDisabled: '=',
+            jfSelectChange: '&?',
+            jfSelectDisplayAttr: '@',
+            jfSelectDisplayFunc: '&?',
+            jfSelectFilterAttr: '@',
+            jfSelectPlaceholder: '@',
+            jfSelectLoadChunks: '@?',
+            jfSelectLoadMoreLabel: '@?',
+            jfSelectHelpTooltips: '&?'
+        });
+
+        this.createWrapper('jf-tabs','jfrog-tabs',{
+            tabs: '=',
+            dictionary: '=',
+            onTabChange: '&(tab)',
+            tabWidth: '@',
+            containerMargin: '@'
+        },true);
+
+        this.createWrapper('jf-tab','jfrog-tab',{ //does not work good
+            name: '@'
+        });
+
+        this.createWrapper('jf-clip-copy','jfrog-clip-copy',{
+            textToCopy: '=',
+            objectName: '@'
+        });
+
+        this.createWrapper('jf-actions','jfrog-actions',{
+            parentController: '=',
+            label: '@',
+            initMethod: '@',
+            fixedActionsNames: '='
+        });
+
+        this.createWrapper('jf-code-mirror','jfrog-code-mirror',{
+            mimeType: '@',
+            mode: '@',
+            model: '=',
+            allowEdit: '@',
+            height: '@?',
+            apiAccess: '=',
+            autofocus: '@'
+        });
+
+        this.createWrapper('jf-sidebar','jfrog-sidebar', {
+            driver: '=',
+            footerTemplate: '@',
+            openAdminSize: '@?',
+            noSearchBox: '@?',
+            externalGotoRoute: '&?($event)'
+        })
+
+        this.createWrapper('jf-marquee','jfrog-marquee', {
+        },true)
+
+        this.createWrapper('toaster-container','jfrog-toaster-container',{
+        });
+    }
+
+    private createWrapper(directiveName:string, wrapperName:string, bindings:Object, transclude:boolean = false) {
+
+        let templateBindings = '';
+
+        for (let bindKey in bindings) {
+            let bindType = bindings[bindKey];
+            let bindExpr;
+            switch (bindType) {
+                case '=': case '=?': {
+                    bindExpr = `$ctrl.${bindKey}`;
+                    break;
+                }
+                case '@': case '@?': {
+                    bindExpr = `{{$ctrl.${bindKey}}}`;
+                    break;
+                }
+                case '&': case '&?': {
+                    bindExpr = `$ctrl.${bindKey}()`;
+                    break;
+                }
+                default: {
+                    if (bindType.startsWith('&') || bindType.startsWith('&?')) {
+                        let params = bindType.match(/\((.*)\)/)[0];
+                        bindExpr = `$ctrl.${bindKey}${params}`;
+                    }
+                    else {
+                        console.log(`Unhandled bind type for ${directiveName}: ${bindType}`)
+                    }
+                }
+            }
+
+            templateBindings += ` ${_.kebabCase(bindKey)}="${bindExpr}"`
+        }
+
+        let transclusion = transclude ? '<ng-transclude></ng-transclude>' : '';
+        let template = `<${directiveName}${templateBindings}>${transclusion}</${directiveName}>`
+
+        let controller = function($element) {
+
+            this.$onInit = () => {
+                for (let bindKey in bindings) {
+                    let bindType = bindings[bindKey];
+                    if ((bindType === '=' || bindType === '=?') && typeof this[bindKey] === 'function') {
+                        this[bindKey] = this[bindKey]() || undefined;
+                    }
+                }
+            }
+        };
+
+        let cleanBindings = (bindings) => {
+            let clean = {};
+            for (let key in bindings) {
+                if (bindings[key].indexOf('(') !== -1) {
+                    clean[key] = bindings[key].substr(0,bindings[key].indexOf('('))
+                }
+                else {
+                    clean[key] = bindings[key];
+                }
+            }
+            return clean;
+        };
+
+        let componentObject = {
+            bindings: cleanBindings(bindings),
+            template,
+            controller,
+            transclude: true
+        };
+        angular.module(NG1_MODULE).component(wrapperName, componentObject);
+
+        let wrapper = this.upgradeAdapter.upgradeNg1Component(wrapperName);
+
+        this.wrappers.push(wrapper);
+
+        return wrapper;
+    }
+}
+
+/*
+let CodeMirror = (window as any).CodeMirror;
+
+function aliasMime(newMime, existingMime) {
+    CodeMirror.defineMIME(newMime, CodeMirror.mimeModes[existingMime]);
+}
+function defineCodeMirrorMimes() {
+    aliasMime('text/x-java-source', 'text/x-java');
+    aliasMime('pom', 'text/xml');
+}
+defineCodeMirrorMimes(); */
