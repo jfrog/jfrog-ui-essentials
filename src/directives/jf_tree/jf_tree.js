88ab744701fe090952218c7fb715e07089ea9fec
dannyr <dannyr@jfrog.com>
2017-04-27 20:45:14 +0300

    jf-tree: Generalizations and improvements

--- src/directives/jf_tree/jf_tree.js
+++ src/directives/jf_tree/jf_tree.js
@@ -8,24 +8,22 @@ class jfTreeController{
     /**
      * Constructor
      * */
-    constructor($scope,$q,$timeout,JFrogEventBus){
+    constructor($scope,$q,$timeout,$element){
         this.$scope = $scope;
         this.$timeout = $timeout;
+        this.$element = $element;
         this.$q = $q;
-        this.JFrogEventBus = JFrogEventBus;
-        this.beautify = require('../../../node_modules/js-beautify/js/index').js_beautify;
         this.currentNode = {};
         this.generatedDictionay = {
             tabs: {
                 result: 'result',
             }
         };
-        this.infoTabs = [{name:'result'}];
-        this.registerTreeEvents();
+//        this.registerTreeEvents();
         this.initTree();
     }
 
-    initTree(){
+    initTree() {
         this.buildTree().then(()=>{
             this.registerTreeEvents();
         });
@@ -42,38 +40,42 @@ class jfTreeController{
         $(this.treeElement).on("select_node.jstree", (e, args) => {
             this.currentNode = args.node;
         });
-    }
-
-    beautifyText (jsonToBeautify) {
-        return this.beautify(jsonToBeautify);
+        $(this.treeElement).on("check_node.jstree", (e, args) => {
+            args.node.data.originalObject.$checked = true;
+        });
+        $(this.treeElement).on("uncheck_node.jstree", (e, args) => {
+            args.node.data.originalObject.$checked = false;
+        });
     }
 
     buildTree(){
         let defer = this.$q.defer();
-        if(!this.jsonData || !this.jsonData.results){
+        if(!this.treeData){
             defer.reject();
             return defer.promise;
         }
-        this.jsonString = this.beautifyText(JSON.stringify(this.jsonData));
-        this.treeData = this.transformData(this.jsonData.results);
+        this.transformedData = this.transformData(this.treeData);
         let TreeConfig = {
             'core': {
                 'data': {}
             },
             "checkbox" : {
-                "keep_selected_style" : false
+                "keep_selected_style" : false,
+                "three_state" : true,
+                "tie_selection" : false,
+                "whole_node": true
             },
             'plugins' : this.plugins
         };
         this.$timeout(()=>{
-            TreeConfig.core.data = this.treeData;
-            this.treeElement = $('#tree-element');
+            TreeConfig.core.data = this.transformedData;
+            this.treeElement = $(this.$element).find('.tree-element');
             $(this.treeElement).jstree(TreeConfig);
             $(this.treeElement).on("ready.jstree", (e) => {
                 let firstNode = $(this.treeElement).find('.jstree-li:first');
                 if(firstNode){
                     let nodeId = firstNode.attr('id');
-                    $(this.treeElement).jstree('select_node', nodeId, true);
+//                    $(this.treeElement).jstree('select_node', nodeId, true);
                     let node = $(this.treeElement).jstree('get_node', nodeId);
                     this.$timeout(()=>{
                         this.currentNode = node;
@@ -85,46 +87,30 @@ class jfTreeController{
         return defer.promise;
     }
 
-    // TODO: Make the transformData method to act as generic as possible by passing mapper functions to this directive that describes each node's name,children,data( but also leave the default)
-    transformData(resultsData,dataText){
-        // // Base case
-        if(!resultsData) return;
-        let i = 0;
-        return _.map(resultsData,(result) => {
-            let item = {};
-            item.data = {};
-            item.children = [];
-            item.text = dataText ? dataText+'-'+i : ""+i;
-            item.icon = "";
-            item.state =  {
-                "opened": false,
-                "disabled": false,
-                "selected": false
-            };
+    jstree() {
+        return $(this.treeElement).jstree();
+    }
 
-            for(let key in result){
-                if (result.hasOwnProperty(key)) {
-                    if(typeof result[key] === 'string'){
-                        item.data[key] = result[key];
-                        this.generatedDictionay[key] = this.toTitleCase(key)
-                    } else if(typeof result[key] === 'object') {
-                        item.children = this.transformData(result[key],key);
-                    }
-                }
-            }
-            i++;
+    transformData(origData){
+        if(!origData || !origData.length) return;
+        return _.map(origData,(origItem) => {
+            let item = {};
+            item.data = {originalObject: _.find(this.treeData,origItem)};
+            item.children = this.transformData(this.getChildrenFunc({item:origItem}) || []);
+            item.text = this.getTextFunc({item:origItem}) || '';
+            item.icon = "jf-tree-no-icon";
+            item.state =  {
+                opened: false,
+                disabled: false,
+                selected: false,
+                checked: false
+            };
             return item;
         });
     }
 
-    toTitleCase(str) {
-        return str.replace('_'," ").replace(/\w\S*/g, (txt)=>{
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    }
-
-    hasData(){
-        return this.jsonData;
+    hasData() {
+        return this.treeData && this.treeData.length;
     }
 
 }
@@ -133,11 +119,13 @@ export function jfTree() {
     return {
         restrict: 'E',
         scope: {
-            jsonData: '=',
+            treeData: '=',
             treeHeader: '=',
             treeWidth: '=',
             treeHeight: '=',
-            plugins: '='
+            plugins: '=',
+            getTextFunc: '&',
+            getChildrenFunc: '&',
         },
         controller: jfTreeController,
         controllerAs: 'jfTree',
