849b13dc28d2522792ab2e9f5bab78f7567d9e5a
matang <matang@jfrog.com>
2017-05-04 15:05:12 +0300

    Added shorten number filter

---
+++ src/filters/formatedNumber.js
@@ -0,0 +1,29 @@
+export function FormatedNumber() {
+
+    return function(value) {
+        var newValue = value;
+        if (value >= 1000) {
+            var suffixes = ["", "k", "m", "b","t"];
+            var suffixNum = Math.floor( (""+value).length/3 );
+            var shortValue = '';
+            for (var precision = 2; precision >= 1; precision--) {
+                shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+                var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+                if (dotLessShortValue.length <= 2) { break; }
+            }
+            if (shortValue % 1 != 0)  shortNum = shortValue.toFixed(1);
+            newValue = shortValue+suffixes[suffixNum];
+        }
+        return newValue;
+    };
+
+
+    /*return function(bytes, precision) {
+        if (bytes===0) return 'empty file'
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+        if (typeof precision === 'undefined') precision = 1;
+        var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+    }*/
+}
--- src/filters/jfrog.filters.module.js
+++ src/filters/jfrog.filters.module.js
@@ -1,6 +1,8 @@
 import {FileSize} from './filesize';
+import {FormatedNumber} from './formatedNumber';
 import {SplitWords} from './split_words';
 
 export default angular.module('jfrog.ui.essentials.filters', [])
         .filter('filesize', FileSize)
+        .filter('formatedNumber', FormatedNumber)
         .filter('splitWords', SplitWords);
