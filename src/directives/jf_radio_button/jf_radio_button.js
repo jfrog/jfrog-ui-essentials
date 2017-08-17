2b3a2a5e1de7d0bd8fd720648345af29447b7e82
dannyr <dannyr@jfrog.com>
2017-08-17 13:52:37 +0300

    jf-radio-button: Support radio-template named transclusion

--- src/directives/jf_radio_button/jf_radio_button.js
+++ src/directives/jf_radio_button/jf_radio_button.js
@@ -1,17 +1,27 @@
 class jfRadioButtonController {
-    constructor($element, $transclude, $timeout) {
+    constructor($element, $transclude, $timeout, $scope) {
         $transclude(function(clone) {
             $timeout(function() {
                 $element.find('label').prepend(clone);
             }, 0, false);
         });
+
+        $scope.onClickTemplate = () => {
+            $timeout(()=>{
+	            $($element).parent().find('input[type=radio]').prop( "checked", false );
+	            $($element).find('input[type=radio]').prop( "checked", true )
+            })
+        }
     }
+
 }
 
 export function jfRadioButton() {
     return {
         restrict: 'E',
-        transclude: true,
+        transclude: {
+            template: '?radioTemplate'
+        },
         scope: {
             text: '@?',
             helper: '@?'
