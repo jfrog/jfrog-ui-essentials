aea01098eb50b3a3b5b1af3cf7de72ba59bd5bb1
dannyr <dannyr@jfrog.com>
2017-08-29 16:23:46 +0300

    Update auto source converters for jfTableView

--- jfTableViewConverter/convert.js
+++ jfTableViewConverter/convert.js
@@ -5,7 +5,12 @@ let jetpack = require('fs-jetpack');
 let _ = require('lodash');
 
 let inspect = (obj) => console.dir(obj, {depth: null});
-let src = (node) => escodegen.generate(node, {comment: true});
+let src = (node, indent = 0) => {
+  let options = {}
+  options.comment = true;
+  if (indent) options.format = {indent: {base: indent}};
+  return escodegen.generate(node, options);
+}
 let json = (node) => console.log(JSON.stringify(node, null, 4));
 
 let directiveRegex = /<jf-grid ((?:.|\n)*?)><\/jf-grid>/g;
@@ -39,46 +44,11 @@ else {
     let newTemplate = rewriteTemplate(template, directives);
     localJetpack.write(conversionJson.overwrite ? templateFile : templateFile.replace('.html', '.converted.html'), newTemplate);
 
-    let newController = rewriteController(controller, directives);
-    localJetpack.write(conversionJson.overwrite ? controllerFile : controllerFile.replace('.js', '.converted.js'), newController);
+    rewriteController(controller, directives);
+    localJetpack.write(conversionJson.overwrite ? controllerFile : controllerFile.replace('.js', '.converted.js'), global.jfTableViewConverter.controllerSource);
 
   }
 }
-
-function _traverse(obj, callback, parents = []) {
-  let stopped = false;
-  if (obj && obj.type) parents.push(obj);
-  _.forIn(obj, (val, key) => {
-    if (stopped) return;
-    if (val && val.type) {
-      let ret = callback(key, val, obj, parents);
-      if (ret === false) stopped = true;
-    }
-    if (!stopped) {
-      if (_.isArray(val) || _.isObject(val)) {
-        _traverse(val, callback, parents);
-      }
-    }
-  });
-}
-
-function _getNodeParent(root, node, type) {
-  let parent = null;
-  _traverse(root, (key, val, p, pArray) => {
-    if (val === node) {
-      if (type) {
-        parent = _.findLast(pArray, {type});
-      }
-      else {
-        parent = p;
-      }
-      return false;
-    }
-  })
-  return parent;
-}
-
-
 
 function getDirectives(template) {
 
@@ -121,42 +91,50 @@ function getAttributesFromElement(element) {
 
 function rewriteTemplate(template, directives) {
   directives.forEach(dir => {
+    let index = template.indexOf(dir.exactElement);
+    let indent = _getIndent(index, template);
     let retainedAttrs = _.filter(Object.keys(dir.attrs), a => !_.includes(convertedAttributes, a));
     retainedAttrs = _.map(retainedAttrs, a => {
       let str = `${a}=${dir.attrs[a]}`
       delete dir.attrs[a];
-      return str;
+      return '\n' + ' '.repeat(indent + '<jf-table-view '.length) + str;
     }).join(' ');
 
-    let newDir = `<jf-table-view options=${dir.attrs['grid-options']}\n${retainedAttrs}></jf-table-view>`
+    let newDir = `<jf-table-view options=${dir.attrs['grid-options']}${retainedAttrs}></jf-table-view>`
     template = template.replace(dir.exactElement, newDir);
   })
   return template;
 }
 
 function rewriteController(controller, directives) {
-  let module = esprima.parseModule(controller, {range: true, tokens: true, comment: true});
-  attachComments(module)
+  global.jfTableViewConverter.theModule = esprima.parseModule(controller, {range: true, tokens: true, comment: true});
 
-  replaceInjections(module);
+  attachComments()
 
-  rewriteDirective(module, directives);
+  replaceInjections();
 
-  return escodegen.generate(module, {comment: true});
+  rewriteDirective(directives);
+
+//  return escodegen.generate(module, {comment: true});
 }
 
-function replaceInjections(module) {
-  let constructor = es$(module, 'ClassDeclaration MethodDefinition[kind="constructor"] FunctionExpression');
+function replaceInjections() {
+  let constructor = es$(global.jfTableViewConverter.theModule, 'ClassDeclaration MethodDefinition[kind="constructor"] FunctionExpression');
   let params = constructor[0].params;
 
+  let foundJFrogGridFactory = false;
   _.remove(params, p => {
+    if (p.name === 'JFrogGridFactory') foundJFrogGridFactory = true;
     return _.includes(['JFrogGridFactory', 'uiGridConstants', 'commonGridColumns'], p.name);
   })
+
+  if (!foundJFrogGridFactory) return;
 
   params.push({
     type: 'Identifier',
     name: 'JFrogTableViewOptions'
   })
+
   let body = constructor[0].body.body;
 
   _.remove(body, statement => {
@@ -180,10 +158,13 @@ function replaceInjections(module) {
     }
   })
 
+  _rewriteNode(_getNodeParent(constructor[0], 'MethodDefinition'));
+
 }
 
-function attachComments(module) {
-  module.body.forEach(part => {
+function attachComments() {
+  let module = global.jfTableViewConverter.theModule;
+  global.jfTableViewConverter.theModule.body.forEach(part => {
     if (part.type === 'ExportNamedDeclaration') {
       escodegen.attachComments(part.declaration, module.comments, module.tokens);
     }
@@ -193,34 +174,34 @@ function attachComments(module) {
   })
 }
 
-function rewriteDirective(module, directives) {
-  let controllerAs = getControllerAs(module);
+function rewriteDirective(directives) {
+  let controllerAs = getControllerAs();
   directives.forEach(dir => {
     let gridOptions = dir.attrs['grid-options'];
     gridOptions = _.trim(gridOptions, '"');
     gridOptions = gridOptions.replace(controllerAs + '.', '');
-    let creationLocation = locateGridCreation(module, gridOptions);
-    rewriteCreation(module, creationLocation, dir);
-    rewriteColumns(module, dir);
-    rewriteAllTheRest(module, dir);
+    let creationLocation = locateGridCreation(gridOptions);
+    rewriteCreation(creationLocation, dir);
+    rewriteColumns(dir);
+    rewriteAllTheRest(dir);
   })
 }
 
-function getControllerAs(module) {
+function getControllerAs() {
   if (global.jfTableViewConverter.conversionJson.controllerAs) {
     return global.jfTableViewConverter.conversionJson.controllerAs
   }
   else {
-    let exportDeclaration = _.find(module.body, {type: 'ExportNamedDeclaration'})
+    let exportDeclaration = _.find(global.jfTableViewConverter.theModule.body, {type: 'ExportNamedDeclaration'})
     let dirDescObject = exportDeclaration.declaration.body.body[0].argument.properties;
     let controllerAsProperty = _.find(dirDescObject, {key: {name: 'controllerAs'}})
     return controllerAsProperty.value.value;
   }
 }
 
-function locateGridCreation(module, gridOptions) {
+function locateGridCreation(gridOptions) {
 
-  let methods = es$(module, 'ClassDeclaration MethodDefinition');
+  let methods = es$(global.jfTableViewConverter.theModule, 'ClassDeclaration MethodDefinition');
 
   let assigment;
   let creationMethod = _.find(methods, method => {
@@ -242,7 +223,8 @@ function locateGridCreation(module, gridOptions) {
 }
 
 
-function rewriteCreation(module, creationLocation, directive) {
+function rewriteCreation(creationLocation, directive) {
+  if (!creationLocation.assigment) return;
   let creation = es$(creationLocation.assigment.right, 'MemberExpression[object.callee.property.name="getGridInstance"]')[0];
   let newExpression = {
     'type': 'NewExpression',
@@ -259,14 +241,14 @@ function rewriteCreation(module, creationLocation, directive) {
     },
     'arguments': creation.object.arguments
   }
-  let expression = _getNodeParent(module, creationLocation.assigment, 'ExpressionStatement');
+  let expression = _getNodeParent(creationLocation.assigment, 'ExpressionStatement');
   let clone = _.cloneDeep(expression);
 
   clone.expression.right = newExpression;
   expression.expression = expression.expression.right;
 
   let chainStart = expression.expression.callee;
-  _removeFromChain(module, chainStart, 'setRowTemplate')
+  _removeFromChain(chainStart, 'setRowTemplate')
 
   let setButtons = es$(chainStart, 'Identifier[name="setButtons"]')[0];
   if (setButtons) setButtons.name = 'setActions';
@@ -275,7 +257,7 @@ function rewriteCreation(module, creationLocation, directive) {
   let setSingleSelect = es$(chainStart, 'Identifier[name="setSingleSelect"]')[0];
 
   if (setMultiSelect || setSingleSelect) {
-    let selectionParent = _getNodeParent(module, _getNodeParent(module, setMultiSelect || setSingleSelect));
+    let selectionParent = _getNodeParent(_getNodeParent(setMultiSelect || setSingleSelect));
 
     selectionParent.callee.property.name = 'setSelection';
     let params = es$(esprima.parse(src(clone.expression.left) + (setMultiSelect ? '.MULTI_SELECTION' : '.SINGLE_SELECTION')), 'ExpressionStatement > MemberExpression')[0];
@@ -304,7 +286,7 @@ function rewriteCreation(module, creationLocation, directive) {
     ]);
   }
 
-  let setColumns = _getNodeParent(module, _getNodeParent(module, es$(expression.expression, 'Identifier[name="setColumns"]')[0]));
+  let setColumns = _getNodeParent(_getNodeParent(es$(expression.expression, 'Identifier[name="setColumns"]')[0]));
 
   let getColumnsMethodName = setColumns.arguments[0].callee.property.name;
 
@@ -312,10 +294,10 @@ function rewriteCreation(module, creationLocation, directive) {
 
   let root = _getChainRoot(chainStart);
   root.object = clone.expression;
-
+  _rewriteNode(creationLocation.assigment)
 }
 
-function rewriteColumns(module, directive) {
+function rewriteColumns(directive) {
   let filterBy = [];
   if (directive.attrs['filter-field'] !== undefined) {
     filterBy.push(_.trim(directive.attrs['filter-field'], '"'));
@@ -324,15 +306,17 @@ function rewriteColumns(module, directive) {
     }
   }
 
-  let getColumnsMethod = es$(module, `ClassDeclaration MethodDefinition[kind="method"][key.name="${directive.getColumnsMethodName}"]`)[0];
+  let getColumnsMethod = es$(global.jfTableViewConverter.theModule, `ClassDeclaration MethodDefinition[kind="method"][key.name="${directive.getColumnsMethodName}"]`)[0];
 
   let arrayExpressions = es$(getColumnsMethod, 'FunctionExpression ArrayExpression');
 
   let columnsArray = _.filter(arrayExpressions, ae => {
-    let parentAE = _getNodeParent(module, ae, 'ArrayExpression');
-    let parentAEMethod = _getNodeParent(module, parentAE, 'MethodDefinition');
+    let parentAE = _getNodeParent(ae, 'ArrayExpression');
+    let parentAEMethod = _getNodeParent(parentAE, 'MethodDefinition');
     return !parentAE || (parentAEMethod !== getColumnsMethod);
   })[0]
+
+  if (!columnsArray) return;
 
   columnsArray.elements.forEach(columnDef => {
     let field = _.find(columnDef.properties, {key: {name: 'field'}});
@@ -388,34 +372,58 @@ function rewriteColumns(module, directive) {
 
   })
 
+  _rewriteNode(columnsArray);
+
 
 }
 
-function rewriteAllTheRest(module, dir) {
-  let controllerAs = getControllerAs(module);
+function rewriteAllTheRest(dir) {
+  let controllerAs = getControllerAs();
   let gridOptions = dir.attrs['grid-options'];
   gridOptions = _.trim(gridOptions, '"');
   gridOptions = gridOptions.replace(controllerAs + '.', '');
 
 
-  let setGridDataCalls = es$(module, 'Identifier[name="setGridData"]');
+  let setGridDataCalls = es$(global.jfTableViewConverter.theModule, 'Identifier[name="setGridData"]');
 
-  setGridDataCalls.forEach(call => {
-    let parent = _getNodeParent(module, call);
+  while (setGridDataCalls.length) {
+    let call = setGridDataCalls[0];
+    let parent = _getNodeParent(call);
 
     if (parent.object.property.name === gridOptions) {
       call.name = 'setData';
     }
-  })
 
-  let getSelectedRowsCalls = es$(module, 'Identifier[name="getSelectedRows"]');
+    _rewriteNode(parent);
 
-  getSelectedRowsCalls.forEach(call => {
-    let parent = _getNodeParent(module, call);
-    if (parent.object && parent.object.object && parent.object.object.object && parent.object.property.name === 'selection' && parent.object.object.property.name === 'api') {
-      parent.object = parent.object.object.object;
-    }
-  })
+    setGridDataCalls = es$(global.jfTableViewConverter.theModule, 'Identifier[name="setGridData"]');
+  }
+
+  let findGetSelectedRowsCalls = () => {
+    let getSelectedRowsCalls = es$(global.jfTableViewConverter.theModule, 'Identifier[name="getSelectedRows"]');
+
+    getSelectedRowsCalls = _.filter(getSelectedRowsCalls, call => {
+      let parent = _getNodeParent(call);
+      if (parent.object && parent.object.object && parent.object.object.object && parent.object.property.name === 'selection' && parent.object.object.property.name === 'api') {
+        return true;
+      }
+    })
+
+    return getSelectedRowsCalls;
+  }
+
+  let getSelectedRowsCalls = findGetSelectedRowsCalls();
+
+  while (getSelectedRowsCalls.length) {
+    let call = getSelectedRowsCalls[0];
+    let parent = _getNodeParent(call);
+
+    parent.object = parent.object.object.object;
+
+    _rewriteNode(parent);
+
+    getSelectedRowsCalls = findGetSelectedRowsCalls();
+  }
 
 }
 
@@ -427,13 +435,13 @@ function _getChainRoot(chainStart) {
   return curr;
 }
 
-function _removeFromChain(module, chainStart, toRemove) {
+function _removeFromChain(chainStart, toRemove) {
   let curr = chainStart;
   let prev;
   while (curr.object.callee) {
     if (curr.property && curr.property.name === toRemove) {
       if (!prev) {
-        let parent = _getNodeParent(module, chainStart);
+        let parent = _getNodeParent(chainStart);
         parent.callee = curr.object.callee;
         parent.arguments = curr.object.arguments;
       }
@@ -455,15 +463,63 @@ function _addToChain(chainStart, methodName, argumentsNode) {
   chainStart.arguments = argumentsNode;
 }
 
-
-/*
-function _rewriteElement(element) {
-  let [start, end] = statement.range;
-  console.log(global.jfTableViewConverter.controllerSource.substr(start, end - start))
-
+function _traverse(obj, callback, parents = []) {
+  let stopped = false;
+  if (obj && obj.type) parents.push(obj);
+  _.forIn(obj, (val, key) => {
+    if (stopped) return;
+    if (val && val.type) {
+      let ret = callback(key, val, obj, parents);
+      if (ret === false) stopped = true;
+    }
+    if (!stopped) {
+      if (_.isArray(val) || _.isObject(val)) {
+        _traverse(val, callback, parents);
+      }
+    }
+  });
 }
-*/
+
+function _getNodeParent(node, type) {
+  let parent = null;
+  _traverse(global.jfTableViewConverter.theModule, (key, val, p, pArray) => {
+    if (val === node) {
+      if (type) {
+        parent = _.findLast(pArray, {type});
+      }
+      else {
+        parent = p;
+      }
+      return false;
+    }
+  })
+  return parent;
+}
 
 
+function _rewriteNode(node) {
+  if (!node.range) {
+    console.error('Trying to rewrite AST node without range data !');
+    return;
+  }
+  let [start, end] = node.range;
 
+  let beforeChange = global.jfTableViewConverter.controllerSource.substr(0, start);
+  let afterChange = global.jfTableViewConverter.controllerSource.substr(end);
 
+  let newCode = src(node, Math.round(_getIndent(start) / 4));
+  newCode = newCode.replace(/\)\./g, ')\n' + ' '.repeat(4 + _getIndent(start)) + '.');
+  global.jfTableViewConverter.controllerSource = beforeChange + newCode + afterChange;
+  global.jfTableViewConverter.theModule = esprima.parseModule(global.jfTableViewConverter.controllerSource, {range: true, tokens: true, comment: true});
+  attachComments()
+}
+
+function _getIndent(offset, src) {
+  let before = (src || global.jfTableViewConverter.controllerSource).substr(0, offset);
+  let lastNewLineIndex = before.lastIndexOf('\n') + 1;
+  let count = 0;
+  while (before.charAt(lastNewLineIndex + count) === ' ' || before.charAt(lastNewLineIndex + count) === '\t') {
+    count++;
+  }
+  return count;
+}
