7e7a9b241e5f0b01b755ef67951b00eeffe71b4f
tomere <tomere@jfrog.com>
2017-09-19 12:53:12 +0300

    JR-369 Files Tab  - Fixes to datetime picker   - Changed datepicker component to directive   - Added support to bind an 'isOpen' flag   - Added triggering from input

--- src/directives/jf_datetimepicker/jf_datetimepicker.js
+++ src/directives/jf_datetimepicker/jf_datetimepicker.js
@@ -2,10 +2,34 @@
  * Created by tomere on 17/09/2017.
  */
 
-export default {
-	bindings: {
-		model: '=',
-		options: '<?'
-	},
-	templateUrl: 'directives/jf_datetimepicker/jf_datetimepicker.html'
+class jfDatetimepickerController {
+	/* @ngInject */
+	constructor() {}
+	$onInit(){
+		this.dpOpitons = {allowInputToggle : true};
+		if(this.options){
+			this.dpOpitons = _.assign({}, this.options , this.dpOpitons);
+		}
+	}
+	onBlur(){
+		this.isDatepickerOpen = false;
+	}
+	onFocus(){
+		this.isDatepickerOpen = true;
+	}
+}
+
+export function jfDatetimepicker() {
+	return {
+		restrict   : 'E',
+		scope      : {
+			isDatepickerOpen: '=?',
+			model           : '=',
+			options         : '<?' // Options are detailed in: http://eonasdan.github.io/bootstrap-datetimepicker/Options/
+		},
+		controller : jfDatetimepickerController,
+		controllerAs: 'jfDatetimepicker',
+		bindToController: true,
+		templateUrl: 'directives/jf_datetimepicker/jf_datetimepicker.html'
+	}
 }
--- src/directives/jfrog.directives.module.js
+++ src/directives/jfrog.directives.module.js
@@ -33,7 +33,7 @@ import {jfClearErrors} from "./jf_clear_errors/jf_clear_errors";
 import {jfMarkupEditor} from "./jf_markup_editor/jf_markup_editor";
 import {jfTree} from "./jf_tree/jf_tree";
 import {jfAutoComplete} from "./jf_auto_complete/jf_auto_complete";
-import jfDatetimepicker from "./jf_datetimepicker/jf_datetimepicker";
+import {jfDatetimepicker} from "./jf_datetimepicker/jf_datetimepicker";
 
 angular.module('jfrog.ui.essentials.directives', [])
     .directive({
@@ -71,6 +71,6 @@ angular.module('jfrog.ui.essentials.directives', [])
         'jfMarkupEditor': jfMarkupEditor,
         'jfTree': jfTree,
         'jfAutoComplete': jfAutoComplete,
-	    'jfRadioButton': jfRadioButton
-    })
-    .component('jfDatetimepicker', jfDatetimepicker);
+	    'jfRadioButton': jfRadioButton,
+        'jfDatetimepicker': jfDatetimepicker
+    });
