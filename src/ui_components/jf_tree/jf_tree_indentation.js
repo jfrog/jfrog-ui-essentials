5caafa198a28e6e6dc994ede4f3f4385f64fe85b
dannyr <dannyr@jfrog.com>
2017-12-15 02:34:04 +0200

    jf-tree: Indentation lines - Build the inner html for the indentation units in js, and use ng-bind-html. it is faster than using ng-repeat in the template.

--- src/ui_components/jf_tree/jf_tree_indentation.js
+++ src/ui_components/jf_tree/jf_tree_indentation.js
@@ -1,7 +1,19 @@
 class jfTreeIndentationController {
 
-    constructor() {
+    constructor($scope, $sce) {
+        $scope.$watch('jfTreeIndentation.indentation', () => {
+            // Build the inner html for the units here in js, it is much faster than using ng-repeat
+            let indentationHtml = '';
+            let htmlForIndentation = (indentation) => {
+                return `<div class="indentation-unit" style="height: ${this.height}; background-image: url('${this.linesBackgrounds[indentation.background]}')"></div>`;
+            }
+            this.indentation.forEach(indentation => {
+                indentationHtml += htmlForIndentation(indentation);
+            })
+            this.indentationHtml = $sce.trustAsHtml(indentationHtml);
+        })
     }
+
 
 }
 export function jfTreeIndentation() {
