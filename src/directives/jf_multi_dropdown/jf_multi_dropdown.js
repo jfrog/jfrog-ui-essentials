219bebe1179a41f90dd24dcb392a2f0d25afe2d1
tomere <tomere@jfrog.com>
2018-02-05 18:25:51 +0200

    RTFACT-15602 Native UI  - Added styling for icon column in summary row  - Enabled passing width to a summery column  - Changed empty placeholder bg color

--- src/directives/jf_multi_dropdown/jf_multi_dropdown.js
+++ src/directives/jf_multi_dropdown/jf_multi_dropdown.js
@@ -1,170 +1,215 @@
 class jfMultiDropdownController {
 	/* @ngInject */
-    constructor($scope, $filter, $element) {
+	constructor($scope, $filter, $element) {
 
-        this.$scope = $scope;
-        this.$element = $element;
+		this.$scope = $scope;
+		this.$element = $element;
 
-        this.filter = $filter('filter');
-        this.opened = false;
+		this.filter = $filter('filter');
+		this.opened = false;
 
-        this.singleSelectionIndex = -1;
+		this.singleSelectionIndex = -1;
 
-        this.handleOutsideClick();
+		this.handleOutsideClick();
 
-        this.$scope.$watch('jfMultiDropdown.items', (newVal, oldVal) => {
-            if (newVal) {
-                this.sortItems();
-                if (this.singleSelection) {
-                    this.items.forEach((item, index) => item.$id = index);
-                    let disabled = _.filter(this.items, item => item.disabled);
-                    disabled.forEach(item => item.isSelected = false);
-                    let selected = _.filter(this.items, item => item.isSelected);
-                    if (selected.length > 1) {
-                        selected.slice(1).forEach(item => item.isSelected = false);
-                    }
-                    if (selected.length) this.singleSelectionIndex = selected[0].$id;
-                }
-            }
-        });
-        this.$scope.$watch('jfMultiDropdown.dropdownOpened', (val) => {
-            if (val === true) {
-                if (!this.items) return;
-                this.opened = true;
-                this.filterText = '';
-            }
-            else if (val === false) {
-                this.opened = false;
-                this.filterText = '';
-            }
-            else {
+		this.$scope.$watch('jfMultiDropdown.items', (newVal, oldVal) => {
+			if (newVal) {
+				this.sortItems();
+				if (this.singleSelection) {
+					this.items.forEach((item, index) => item.$id = index);
+					let disabled = _.filter(this.items, item => item.disabled);
+					disabled.forEach(item => item.isSelected = false);
+					let selected = _.filter(this.items, item => item.isSelected);
+					if (selected.length > 1) {
+						selected.slice(1).forEach(item => item.isSelected = false);
+					}
+					if (selected.length) {
+						this.singleSelectionIndex = selected[0].$id;
+					}
+				}
+			}
+		});
+		this.$scope.$watch('jfMultiDropdown.dropdownOpened', (val) => {
+			if (val === true) {
+				if (!this.items) {
+					return;
+				}
+				this.opened = true;
+				this.filterText = '';
+			}
+			else if (val === false) {
+				this.opened = false;
+				this.filterText = '';
+			}
+			else {
 
-            }
-        });
+			}
+		});
 
-	    this.isAllSelected = false;
-	    this.showSelectAllCheckbox = this.items && this.items.length && this.enableSelectAllCheckbox && !this.singleSelection;
+		this.isAllSelected = false;
+		this.showSelectAllCheckbox = this.items && this.items.length && this.enableSelectAllCheckbox && !this.singleSelection;
 
-    }
+	}
 
-    sendOpenStateChange() {
-        if (this.onOpenStateChange) this.onOpenStateChange({opened:this.opened});
-    }
+	sendOpenStateChange() {
+		if (this.onOpenStateChange) {
+			this.onOpenStateChange({opened: this.opened});
+		}
+	}
 
-    handleOutsideClick() {
-        let handler = (e) => {
-            let outside = !$(e.target).parents('.jf-multi-dropdown').length || $(e.target).parents('.jf-multi-dropdown')[0] !== $(this.$element).find('.jf-multi-dropdown')[0];
-            if (outside) {
-                this.opened = false;
-                this.sendOpenStateChange();
-                this.sortItems();
-            }
-            this.$scope.$apply();
-        };
-        $(document).on('click',handler);
-        this.$scope.$on('$destroy',() => {
-            $(document).off('click', handler)
-        })
-    }
+	handleOutsideClick() {
+		let handler = (e) => {
+			let outside = !$(e.target).parents('.jf-multi-dropdown').length || $(e.target).parents(
+				'.jf-multi-dropdown')[0] !== $(this.$element).find('.jf-multi-dropdown')[0];
+			if (outside) {
+				this.opened = false;
+				this.sendOpenStateChange();
+				this.sortItems();
+			}
+			this.$scope.$apply();
+		};
+		$(document).on('click', handler);
+		this.$scope.$on('$destroy', () => {
+			$(document).off('click', handler);
+		});
+	}
 
-    getSelectedForTitle() {
-        let selected = _.filter(this.items, (item) => item.isSelected);
-        selected = _.map(selected, s => s.selectedText || s.text);
-        return selected.join(', ')
-    }
+	getSelectedForTitle() {
+		let selected = _.filter(this.items, (item) => item.isSelected);
+		selected = _.map(selected, s => s.selectedText || s.text);
+		return selected.join(', ');
+	}
 
-    onClick() {
-        if (this.disabled !== true) {
-            if (!this.items) return;
-            this.opened = !this.opened;
-            this.sendOpenStateChange();
-            if (!this.opened) this.sortItems();
-            this.filterText = '';
+	onClick() {
+		if (this.disabled !== true) {
+			if (!this.items) {
+				return;
+			}
+			this.opened = !this.opened;
+			this.sendOpenStateChange();
+			if (!this.opened) {
+				this.sortItems();
+			}
+			this.filterText = '';
 
-        }
+		}
 
-    }
+	}
 
-    onSelection(toggleAll) {
-	    this.applyChanges();
-	    if (toggleAll) {
-		    if (this.isAllSelected) {
-			    this.selectAll();
-		    } else {
-			    this.unSelectAll();
-		    }
-	    } else {
-		    this.isAllSelected = false;
-        }
-    }
+	onSelection(toggleAll) {
+		this.applyChanges();
+		if (toggleAll) {
+			if (this.isAllSelected) {
+				this.selectAll();
+			} else {
+				this.unSelectAll();
+			}
+		} else {
+			this.isAllSelected = false;
+		}
+	}
 
-    onSingleSelection() {
-        this.items.forEach((item)=>{if (!item.disabled) item.isSelected = false;});
-        let selected = _.find(this.items, item => item.$id == this.singleSelectionIndex);
-        selected.isSelected = true;
-        if (this.onChange) this.onChange();
-    }
+	onSingleSelection() {
+		this.items.forEach((item) => {
+			if (!item.disabled) {
+				item.isSelected = false;
+			}
+		});
+		let selected = _.find(this.items, item => item.$id == this.singleSelectionIndex);
+		selected.isSelected = true;
+		if (this.onChange) {
+			this.onChange();
+		}
+	}
 
-    getSelectedCount() {
-        let selected = _.filter(this.items, (item) => item.isSelected && !item.isAllToggleCheckbox);
-        return selected.length;
-    }
+	getSelectedCount() {
+		let selected = _.filter(this.items, (item) => item.isSelected && !item.isAllToggleCheckbox);
+		return selected.length;
+	}
 
-    applyChanges() {
-	    if (this.onChange) this.onChange();
-    }
+	applyChanges() {
+		if (this.onChange) {
+			this.onChange();
+		}
+	}
 
-    selectedItems() {
-        let selected = _.filter(this.items, (item) => item.isSelected);
-        selected = _.map(selected, 'text');
-        return selected;
-    }
+	selectedItems() {
+		let selected = _.filter(this.items, (item) => item.isSelected);
+		selected = _.map(selected, 'text');
+		return selected;
+	}
 
-    sortItems() {
-        if (this.noSelectedFirst) return;
-        if (!this.items) return;
-        let selected = this.noSort ? _.filter(this.items, (item) => item.isSelected) : _.sortBy(_.filter(this.items, (item) => item.isSelected), 'text');
-        let unSelected = this.noSort ? _.filter(this.items, (item) => !item.isSelected) : _.sortBy(_.filter(this.items, (item) => !item.isSelected), 'text');
-        this.lastSelectedIndex = selected.length-1;
-        let combined = selected.concat(unSelected);
-        this.items.splice.apply(this.items, [0,this.items.length].concat(combined));
-    }
-    selectAll() {
-        this.filter(this.items, this.filterText).forEach((item)=>{if (!item.disabled) item.isSelected = true;})
-	    this.applyChanges();
-    }
-    unSelectAll() {
-        this.filter(this.items, this.filterText).forEach((item)=>{if (!item.disabled) item.isSelected = false;})
-	    this.applyChanges();
-        this.singleSelectionIndex = -1;
-    }
+	sortItems() {
+		if (this.noSelectedFirst) {
+			return;
+		}
+		if (!this.items) {
+			return;
+		}
+		let selected = this.noSort ? _.filter(this.items, (item) => item.isSelected) : _.sortBy(
+			_.filter(this.items, (item) => item.isSelected), 'text');
+		let unSelected = this.noSort ? _.filter(this.items, (item) => !item.isSelected) : _.sortBy(
+			_.filter(this.items, (item) => !item.isSelected), 'text');
+		this.lastSelectedIndex = selected.length - 1;
+		let combined = selected.concat(unSelected);
+		this.items.splice.apply(this.items, [0, this.items.length].concat(combined));
+	}
+
+	selectAll() {
+		this.filter(this.items, this.filterText).forEach((item) => {
+			if (!item.disabled) {
+				item.isSelected = true;
+			}
+		});
+		this.applyChanges();
+	}
+
+	unSelectAll() {
+		this.filter(this.items, this.filterText).forEach((item) => {
+			if (!item.disabled) {
+				item.isSelected = false;
+			}
+		});
+		this.applyChanges();
+		this.singleSelectionIndex = -1;
+	}
+
+	clearSelection() {
+		if (this.textInputs) {
+			_.forEach(this.items, (item) => {
+				item.inputTextValue = '';
+			});
+		}
+		this.unSelectAll();
+	}
 
 }
 
 export function jfMultiDropdown() {
-    return {
-        controller: jfMultiDropdownController,
-        controllerAs: 'jfMultiDropdown',
-        bindToController: true,
-        scope: {
-            title: '@',
-	        label: '@',
-            filterPlaceholder: '@',
-            noItemsMessage: '@',
-            items: '=',
-            disabled: '=?',
-            onChange: '&?',
-            noSort: '=?',
-            onOpenStateChange: '&?',
-            dropdownOpened: '=',
-            showSelected: '@',
-	        showLabelCounter: '@',
-            noFilter: '=?',
-            noSelectedFirst: '=?',
-            singleSelection: '=?',
-	        textInputs: '<?',
-	        enableSelectAllCheckbox: '<?'
-        },
-        templateUrl: 'directives/jf_multi_dropdown/jf_multi_dropdown.html'
-    }
+	return {
+		controller: jfMultiDropdownController,
+		controllerAs: 'jfMultiDropdown',
+		bindToController: true,
+		scope: {
+			title: '@',
+			label: '@',
+			filterPlaceholder: '@',
+			noItemsMessage: '@',
+			items: '=',
+			disabled: '=?',
+			onChange: '&?',
+			noSort: '=?',
+			onOpenStateChange: '&?',
+			dropdownOpened: '=',
+			showSelected: '@',
+			showLabelCounter: '@',
+			noFilter: '=?',
+			noSelectedFirst: '=?',
+			singleSelection: '=?',
+			textInputs: '<?',
+			enableSelectAllCheckbox: '<?',
+			borderless: '<?'
+		},
+		templateUrl: 'directives/jf_multi_dropdown/jf_multi_dropdown.html'
+	};
 }
--- src/ui_components/jf_summary_row/summary.row.component.js
+++ src/ui_components/jf_summary_row/summary.row.component.js
@@ -11,11 +11,26 @@ export default class JfSummaryRow {
 
 class JfSummaryRowController {
 	$onInit() {
+		this.filterOutUnactiveColumns();
+		this.setColumnsLayout();
+	}
+
+	filterOutUnactiveColumns() {
 		this.columnsToShow = _.filter(this.columns, (col) => {
 			return col.isActive;
 		});
+	}
+
+	setColumnsLayout() {
+		let layout = '';
+		_.forEach(this.columnsToShow, (col) => {
+			if (!col.width) {
+				col.width = '1fr';
+			}
+			layout += `${col.width} `;
+		});
 		this.inlineStyle = {
-			gridTemplateColumns: `repeat(${this.columnsToShow.length}, 1fr)`
+			gridTemplateColumns: layout
 		};
 	}
 }
