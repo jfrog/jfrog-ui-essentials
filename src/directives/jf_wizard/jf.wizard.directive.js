b8489538ddf290122d5bc5b955d7d1ef1e59a239
aviranb <aviranb@jfrog.com>
2018-04-05 10:43:02 +0300

    Version 1.2.0-dev.20 - Pre-Tag

--- src/directives/jf_wizard/jf.wizard.directive.js
+++ src/directives/jf_wizard/jf.wizard.directive.js
@@ -2,31 +2,39 @@ export default function jfWizard() {
 
     return {
         restrict: 'E',
-        scope: {},
-	    templateUrl: 'directives/jf_wizard/jf.wizard.view.html',
+        scope: {
+            onTabSwitch: '&?',
+        },
+        templateUrl: 'directives/jf_wizard/jf.wizard.view.html',
         controller: jfWizardController,
         controllerAs: '$ctrl',
         transclude: true,
         link: ($scope, element, attrs) => {
+
         }
     }
 }
 
 class jfWizardController {
-    constructor() {
-        this.init=true;
-        this.tabs=[];
+    constructor($scope) {
+        this.$scope = $scope;
+        this.onTabSwitch = $scope.onTabSwitch;
+        this.init = true;
+        this.tabs = [];
     }
 
-    registerTab(item){
-        if(this.init){
-            this.active=item;
-            this.init=false;
+    registerTab(item) {
+        if (this.init) {
+            this.active = item;
+            this.init = false;
         }
         this.tabs.push(item);
     }
 
-    switch(tab){
+    switch(tab) {
         this.active = tab;
+        if (this.onTabSwitch) {
+            this.onTabSwitch({tab: tab})
+        }
     }
 }
