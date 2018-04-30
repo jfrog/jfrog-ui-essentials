144c63534d5df95267db3bb26b77f59b73bf57d1
tomere <tomere@jfrog.com>
2018-04-30 15:00:36 +0300

    - jf-wizard enable triggering tab change event programmatically [AsafN] - jf-table-top hide filter bug fix

--- src/constants/jfrog_ui_lib_events.constants.js
+++ src/constants/jfrog_ui_lib_events.constants.js
@@ -1,21 +1,23 @@
 let events = {
-    TABS_REFRESH:                   'tabs:refresh',
-    TABS_URL_CHANGED:               'tabs:url:changed',
+	TABS_REFRESH: 'tabs:refresh',
+	TABS_URL_CHANGED: 'tabs:url:changed',
 
-    FORM_CLEAR_FIELD_VALIDATION:    'form:clear:field',
-    FORM_SUBMITTED:                 'form:submitted',
+	FORM_CLEAR_FIELD_VALIDATION: 'form:clear:field',
+	FORM_SUBMITTED: 'form:submitted',
 
-    CLOSE_MODAL:                    'modal:close',
+	CLOSE_MODAL: 'modal:close',
 
-    CLOSE_NOTIFICATIONS:            'notifications:close',
+	CLOSE_NOTIFICATIONS: 'notifications:close',
 
-    SIDEBAR_SIZE_CHANGE:            'sidebar:size:change',
+	SIDEBAR_SIZE_CHANGE: 'sidebar:size:change',
 
-    RESET_GRID_PAGINATION:          'reset:grid:pagination',
+	RESET_GRID_PAGINATION: 'reset:grid:pagination',
 
-	TABLEVIEW_HIDE_ACTIONS_DROPDOWN:'tableview:hide:actions:dropdown',
+	TABLEVIEW_HIDE_ACTIONS_DROPDOWN: 'tableview:hide:actions:dropdown',
 
-    CONTEXT_MENU_OPEN :'context:menu:open',
+	CONTEXT_MENU_OPEN: 'context:menu:open',
+
+	WIZARD_TAB_CHANGE: 'wizard:tab:change'
 };
 
 export default events;
