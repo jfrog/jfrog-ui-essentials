4874728a20877889ec0047981ac679ad569b2057
dannyr <dannyr@jfrog.com>
2018-08-23 17:48:28 +0300

    JFUIE-63 - Row actions dropdown button can't be hovered & opened + The drop down is cut by table container
    ** New directive: jf-reparent

---
+++ src/directives/jf_reparent/jf_reparent.js
@@ -0,0 +1,51 @@
+/**
+ * Created by tomere on 29/06/2017.
+ */
+class jfReparentController {
+	/* @ngInject */
+	constructor($element, $timeout) {
+
+		this.$element = $element;
+		this.$timeout = $timeout;
+
+	}
+
+	$onInit() {
+
+        this.$timeout(() => {
+            let target = this.target ? $(this.target) : $(document.body);
+            let offset = this.getOffset(this.$element[0]);
+            let targetOffset = this.getOffset(target[0]);
+            this.$timeout(() => {
+                target.append(this.$element);
+                this.$element.css('position', 'absolute');
+                this.$element.css('left', (offset.left - targetOffset.left) + 'px');
+                this.$element.css('top', (offset.top - targetOffset.top) + 'px');
+                let width = this.$element.width();
+                let height = this.$element.height();
+                this.$element.css('width', width + 'px');
+                this.$element.css('height', height + 'px');
+            })
+        })
+    }
+
+    getOffset(el) {
+        var rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+}
+
+export function jfReparent() {
+
+	return {
+		restrict: 'A',
+		scope: {
+		    target: '=jfReparent'
+        },
+        bindToController: true,
+		controller: jfReparentController
+	};
+}
--- src/directives/jfrog.directives.module.js
+++ src/directives/jfrog.directives.module.js
@@ -49,6 +49,7 @@ import {jfVScroll} from "./jf_vscroll/jf_vscroll";
 import {jfVScrollElement} from "./jf_vscroll/jf_vscroll_element";
 import {jfPendingData} from "./jf_pending_data/jf_pending_data";
 import {jfTextBox} from './jf_text_box/jf_text_box';
+import {jfReparent} from './jf_reparent/jf_reparent';
 import jfWizard from './jf_wizard/jf.wizard.directive';
 import jfWizardElement from './jf_wizard/jf_wizard_element/jf.wizard.element.directive';
 
@@ -106,5 +107,6 @@ angular.module('jfrog.ui.essentials.directives', [])
         'jfSwitchToggler': jfSwitchToggler,
         'jfOnOffSwitch': jfOnOffSwitch,
         'jfTextBox': jfTextBox,
+        'jfReparent': jfReparent,
         'jfPendingData': jfPendingData,
     });
