f5b1a47135ab32053d6d7528e1c32e56d4b3c436
tomere <tomere@jfrog.com>
2018-10-18 16:58:08 +0300

    feature/JFUIE-93 Change pagination edit behavior in jf_table - Added support for deleting the page number and typing bugfix/JFUIE-96 dnd - Change mouse cursor when checking is not available - Added CSS for a not-draggable table/dnd row

--- src/directives/jf_drag_drop_pagination/jf_drag_drop_pagination.js
+++ src/directives/jf_drag_drop_pagination/jf_drag_drop_pagination.js
@@ -4,24 +4,33 @@ class jfDragDropPaginationController {
     }
 
     $onInit() {
-        this.currentPage = 0;
+	    this.currentPage = 0;
+	    this.pageViewModel = 1;
 
-        if (this.paginationApi) {
+	    if (this.paginationApi) {
             this.currentPage = this.paginationApi.getCurrentPage();
+	        this.pageViewModel = this.currentPage;
 
             this.paginationApi.registerChangeListener((pageNum)=>{
                 this.currentPage = pageNum;
+	            this.pageViewModel = this.currentPage;
             });
         }
     }
 
+    onBlur() {
+		this.pageViewModel = parseInt(this.pageViewModel);
+		if(!this.pageViewModel) this.pageViewModel = this.currentPage;
+	}
+
     pageChanged() {
+	    this.pageViewModel = parseInt(this.pageViewModel);
+	    if(this.pageViewModel!== 0 && !this.pageViewModel) return;
 
-        this.currentPage = parseInt(this.currentPage);
-
-        if (!this.currentPage) this.currentPage = 1;
+	    this.currentPage = this.pageViewModel;
         if (this.currentPage < 1) this.currentPage = 1;
         if (this.currentPage > this.paginationApi.getTotalPages()) this.currentPage = this.paginationApi.getTotalPages();
+	    this.pageViewModel =  this.currentPage;
         this.paginationApi.setPage(this.currentPage);
     }
 
