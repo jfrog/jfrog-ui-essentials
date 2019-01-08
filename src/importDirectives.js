d4e52f453d0c33ca1e6e3c8aa3d04d42eeee19cb
dannyr <dannyr@jfrog.com>
2019-01-08 14:56:02 +0200

    Vue version - jf-reveal-input

--- src/importDirectives.js
+++ src/importDirectives.js
@@ -2,3 +2,4 @@ import './directives/JfDynamicTemplateDirective.js';
 import './directives/JfIncludeReplaceDirective.js';
 import './directives/JfTooltipDirective.js';
 import './directives/JfTooltipOnOverflowDirective.js';
+import './directives/JfRevealInputDirective.js';
