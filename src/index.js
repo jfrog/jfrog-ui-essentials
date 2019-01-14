e509a62c5db99815e3a8a2eacff2c35c938cfa30
asafn-jfrog <asafn@jfrog.com>
2019-01-14 17:04:40 +0200

    dev improvements, code splitting bug

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './registerGlobalComponents';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import VueVirtualScroller from 'vue-virtual-scroller';
+import './assets/stylesheets/main.less';
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
+import '../node_modules/jf-tooltipster/css/tooltipster.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import $ from 'jquery';
 import JFrogUI from './plugins/JFrogUI';
-window.$ = $;
-window.jQuery = $;
 import 'jquery-contextmenu';
 import VueClipboard from 'vue-clipboard2'
+window.$ = $;
+window.jQuery = $;
 
-import('./assets/stylesheets/main.less');
-import('../node_modules/jf-tooltipster/css/tooltipster.css');
 require('../node_modules/jf-tooltipster/js/jquery.tooltipster');
 
 export default {
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,31 @@
-module.exports = {
-  runtimeCompiler: true,
-  configureWebpack: {
-    module: {
-      rules: [{
-        test: /\.html$/,
-        use: [ {
-          loader: 'html-loader',
-          options: {
-            minimize: true
-          }
-        }],
-      }]
+const IS_DEV = process.argv.includes('dev');
+
+const config = {
+    runtimeCompiler: true,
+    configureWebpack: {
+        module: {
+            rules: [{
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true
+                    }
+                }]
+            }]
+        }
     }
-  }
+};
+
+if (IS_DEV) {
+    config.chainWebpack = config => {
+        config.devtool(false);
+        config.plugins.delete('hash-module-ids');
+        config.plugins.delete('optimize-css');
+        config.optimization.minimize(false);
+        config.optimization.splitChunks(false);
+    }
 }
+
+
+module.exports = config;
