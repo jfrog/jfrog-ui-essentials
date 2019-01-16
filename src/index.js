cb6651825328906ac0288b48b38e5758248accc9
asafn-jfrog <asafn@jfrog.com>
2019-01-16 15:03:09 +0200

    JFUIE-147 Notifications convert - merge

--- src/index.js
+++ src/index.js
@@ -46,8 +46,6 @@ import './assets/stylesheets/main.less';
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 import '../node_modules/jf-tooltipster/css/tooltipster.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import VueCodemirror from 'vue-codemirror';
-import 'codemirror/lib/codemirror.css'
 import $ from 'jquery';
 import JFrogUI from './plugins/JFrogUI';
 import 'jquery-contextmenu';
