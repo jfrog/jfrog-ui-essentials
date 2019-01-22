69ffc5e179eb5559e28f4f6d502178e91d26c34f
asafn-jfrog <asafn@jfrog.com>
2019-01-22 15:16:23 +0200

    JFUIE-160

--- src/components/JfDividerComponent.vue
+++ src/components/JfDividerComponent.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div>
+    <div class="jf-divider">
         <div class="jf-divider-container">
             <div class="jf-divider-header">{{text}}</div>
             <div class="jf-divider-line"></div>
@@ -17,13 +17,30 @@
         data() {
             return {};
         },
-        ng1_legacy: { 'controllerAs': '$ctrl' }
     }
 
 </script>
 
 <style scoped lang="less">
+    @import '../assets/stylesheets/main';
 
-    
+    .jf-divider {
+        .jf-divider-container {
+            display: flex;
+            margin-bottom: 30px;
+            .jf-divider-header {
+                font-size: 16px;
+                color: @blackBGLight;
+                margin-bottom: -5px;
+                font-weight: 600;
+            }
+            .jf-divider-line {
+                flex: 1;
+                border-bottom: 1px solid @grayBorderLighter;
+                margin-left: 4px;
+            }
+        }
+    }
+
 
 </style>
