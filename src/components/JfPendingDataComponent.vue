a3e8ecd361f92dbffbe6ae43a639d52ae29257db
nirmal93guna <gunaseelann@jfrog.com>
2019-01-28 14:51:08 +0530

    JFUIE-168 jf-pending-data converted to vue

--- src/components/JfPendingDataComponent.vue
+++ src/components/JfPendingDataComponent.vue
@@ -1,10 +1,10 @@
 <template>
 
     <div>
-        <div class="spinner-msg-local" v-if="waitFor === undefined && (!delaySpinner || showSpinner)">
+        <div class="spinner-msg-local" v-if="!waitFor && (!delaySpinner || showSpinner)">
             <div class="icon-hourglass-local"></div>
         </div>
-        <slot v-if="waitFor !== undefined"></slot>
+        <slot v-if="waitFor"></slot>
     </div>
 
 </template>
@@ -17,18 +17,16 @@
             'waitFor',
             'delaySpinner'
         ],
-        'jf@inject': ['$timeout'],
         data() {
             return { showSpinner: null };
         },
         mounted() {
             if (this.delaySpinner) {
-                this.$timeout(() => {
+                setTimeout(() => {
                     this.showSpinner = true;
                 }, !_.isNaN(parseInt(this.delaySpinner)) ? this.delaySpinner : 400);
             }
-        },
-        ng1_legacy: { 'controllerAs': '$ctrl' }
+        }
     }
 
 </script>
