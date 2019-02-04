03ffd42417a55661ae86f08ed6e319b97a8a6575
vivekkodira <vivekkodira@gmail.com>
2019-02-04 14:28:33 +0530

    JFUIE-166 removes redundant code

--- src/directives/JfIncludeReplaceDirective.js
+++ src/directives/JfIncludeReplaceDirective.js
@@ -1,11 +1,6 @@
 const Vue = window.Vue;
 
 Vue.directive('jf-include-replace', {
-    updated: function(el, binding) {
-        console.log(`updated: ${binding && binding.value}`)
-        let element = $(el)
-        element.replaceWith(element.children());
-    },
     inserted: function (el, binding) {
         console.log(`inserted: ${binding && binding.value}`)
         let element = $(el)
