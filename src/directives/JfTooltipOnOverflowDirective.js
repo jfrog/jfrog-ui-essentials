d8174a83691fd14da0e7c0ea818ebfed49d352ba
asafn-jfrog <asafn@jfrog.com>
2019-02-04 19:28:22 +0200

    v-jf-tooltip-on-overflow Demo

--- src/directives/JfTooltipOnOverflowDirective.js
+++ src/directives/JfTooltipOnOverflowDirective.js
@@ -6,10 +6,10 @@ Vue.directive('jf-tooltip-on-overflow', {
         let patchedLinkFn = Ng1AttributeDirectiveAdapter.patchLinkFunction(ng1LinkFunction, null);
         patchedLinkFn(el, binding, vnode);
     }
-})
+});
 
 function ng1LinkFunction($scope, $element, $attrs) {
-    $($element).on('mouseenter',(e)=>{
+    $($element).on('mouseenter', (e) => {
         let targets = [$($element), $(e.target)];
         let tooltipShown = false;
         let isNoTooltip = (cell) => {
@@ -19,13 +19,15 @@ function ng1LinkFunction($scope, $element, $attrs) {
         for (let i = 0; !tooltipShown && i < targets.length; i++) {
             let target = targets[i];
 
-            let targetContent = target.children(':not(:visible)').length ? target.children(':visible').text().trim() : target.text().trim();
+            let targetContent = target.children(':not(:visible)').length ? target.children(':visible')
+                                                                                 .text()
+                                                                                 .trim() : target.text().trim();
             targetContent = (targetContent === '' ? null : targetContent/*$sanitize(targetContent)*/);
             if (!isNoTooltip(target) && target[0].scrollWidth > Math.round(target.innerWidth())) {
                 if (!!targetContent && !target.hasClass('tooltipstered')) {
                     let options = {
                         animation: 'fade',
-                        contentAsHTML : 'true',
+                        contentAsHTML: 'true',
                         trigger: 'hover',
                         onlyOne: 'true',
                         interactive: 'true',
@@ -33,27 +35,27 @@ function ng1LinkFunction($scope, $element, $attrs) {
                         theme: 'tooltipster-default bottom',
                         content: targetContent
                     };
-                    if($scope.trustTooltipText) {
+                    if ($scope.trustTooltipText) {
                         options.contentAsHTML = 'true';
                     }
                     target.tooltipster(options);
                     target.tooltipster('show');
-                }
-                else if (!!targetContent) {
+                } else if (!!targetContent) {
                     target.tooltipster('enable');
 
-                    if (target.tooltipster('content') != targetContent)
+                    if (target.tooltipster('content') != targetContent) {
                         target.tooltipster('content', targetContent);
+                    }
                 }
-            }
-            else if (target.hasClass('tooltipstered'))
+            } else if (target.hasClass('tooltipstered')) {
                 target.tooltipster('disable');
+            }
         }
     });
-/*
-    $scope.$on('$destroy', () => {
-        $($element).off('mouseenter');
-        $($element).off('mouseleave');
-    });
-*/
+    /*
+		$scope.$on('$destroy', () => {
+			$($element).off('mouseenter');
+			$($element).off('mouseleave');
+		});
+	*/
 }
