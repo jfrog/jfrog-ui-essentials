7a203defd542fdfafbde2dbe51bfe5da03569234
nirmal93guna <gunaseelann@jfrog.com>
2019-02-12 14:39:19 +0530

    JFUIE-167 Added snapshot testing to jf-graphs

--- src/components/JfGraphsComponent/index.test.js
+++ src/components/JfGraphsComponent/index.test.js
@@ -1,5 +1,17 @@
 import {shallowMount} from '@vue/test-utils';
 import JfGraphsComponent from './index.vue';
+import { bb } from 'billboard.js';
+
+// Props data for chart
+const options = {
+    data: {
+        columns: [
+            ["data1", 30, 200, 100, 170, 150, 250],
+            ["data2", 130, 100, 140, 35, 110, 50]
+        ]
+    }
+}
+
 
 describe('JfGraphsComponent', () => {
 
@@ -7,21 +19,24 @@ describe('JfGraphsComponent', () => {
         jest.useFakeTimers();
         const wrapper = shallowMount(JfGraphsComponent, {
             propsData: {
-                options: {
-                    data: {
-                        type: "bar",
-                        columns: [
-                            ["data1", 30, 200, 100, 170, 150, 250],
-                            ["data2", 130, 100, 140, 35, 110, 50]
-                        ]
-                    }
-                }
-            },
-            mocks: {
+                options
             }
         });
-        // jest.runAllTimers();
-        expect(wrapper.vm.$el).toMatchSnapshot()
+        // mocking the method to generate graph
+        const _generate = function () {
+            bb.generate(Object.assign({ bindto: wrapper.find({ ref: 'chart' }) }, wrapper.vm.options))
+        }
+        wrapper.setMethods({ _generate })
+        wrapper.vm._generate();
+
+        // to replace auto-generated ids by billboard
+        const idValue = new RegExp(`${wrapper.vm.$el.innerHTML.substr(93, 13)}`, "g");       
+        expect.addSnapshotSerializer({
+            test: (val) => val.innerHTML.replace(idValue, 'test'),
+            print: (val) => `${val.innerHTML.replace(idValue, 'test')}`
+          })
+
+        expect(wrapper.vm.$el).toMatchSnapshot();
     });
 
     it('Contains an element to render graph', () => {
