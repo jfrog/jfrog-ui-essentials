edb17077fdd98ac96b7382ac764728ca71d2d824
nirmal93guna <gunaseelann@jfrog.com>
2019-02-12 18:17:53 +0530

    JFUIE-147 changes to jf-tabs unit tests

--- src/components/JfTabsComponent/index.test.js
+++ src/components/JfTabsComponent/index.test.js
@@ -54,88 +54,73 @@ const $router = {
     replace: jest.fn()
 }
 
+// mount component
+const mountComponent = function ( props ){
+    let options = {
+        localVue,
+        propsData,
+        mocks: {
+            $route,
+            $router,
+            JFrogEventBus
+        },
+        methods: {
+            _calculateTabsSize: function () {
+                this.tabsVisible = _.take(tabArray, tabArray.length)
+            }
+        }
+    }
+    // additional value to be mounted
+    if (props) {
+        if(props.slotValue) Object.assign(options, props.slotValue)
+        if (props.stubValue) Object.assign(options, props.stubValue)   
+        if (props.routeValue == 'tobenull') {
+            options.mocks.$route.query.activeTab = null
+        }
+    }
+    return shallowMount(JfTabsComponent, options);
+}
+
 describe('JfTabsComponent', () => {
+
+    let wrapper;    
     it('All the tabs being rendered', () => {
         jest.useFakeTimers();
-        const wrapper = shallowMount(JfTabsComponent, {
-            propsData,
-            localVue,
-            mocks: {
-                $route,
-                $router,
-                JFrogEventBus
-            },
-            methods: {
-                _calculateTabsSize: function () {
-                    wrapper.vm.tabsVisible = _.take(tabArray, tabArray.length)
-                }
-            }
-        })
+        wrapper = mountComponent();
         jest.runAllTimers();
         expect(wrapper.findAll('li').length).toBe(tabArray.length + 1)
     });
-
+    
     it('active tab is pulled from route params', () => {
         jest.useFakeTimers();
-        const wrapper = shallowMount(JfTabsComponent, {
-            propsData,
-            localVue,
-            mocks: {
-                $route,
-                $router,
-                JFrogEventBus
-            },
-            methods: {
-                _calculateTabsSize: function () {
-                    wrapper.vm.tabsVisible = _.take(tabArray, tabArray.length)
-                }
-            }
-        });
+        wrapper = mountComponent();
         jest.runAllTimers();
         expect(wrapper.vm.currentTab.name).toBe($route.query.activeTab);
     });
-
+    
     it('when route doesnt have params active tab is set from propsData', () => {
         jest.useFakeTimers();
-        const wrapper = shallowMount(JfTabsComponent, {
-            propsData,
-            localVue,
-            mocks: {
-                $route: {
-                    query: {
-                        activeTab: null
-                    }
-                },
-                $router,
-                JFrogEventBus
-            },
-            methods: {
-                _calculateTabsSize: function () {
-                    wrapper.vm.tabsVisible = _.take(tabArray, tabArray.length)
-                }
-            }
-        });
+        wrapper = mountComponent({
+            routeValue: 'tobenull'
+        })
         jest.runAllTimers();
         expect(wrapper.vm.currentTab.name).toBe(wrapper.vm.activeTab);
     });
-
+    
     it('Renders jf-tab from slot', () => {
         jest.useFakeTimers();
-        const wrapper = shallowMount(JfTabsComponent, {
-            propsData,
-            slots: {
-                default: '<jf-tab></jf-tab>'
+        wrapper = mountComponent({
+            slotValue: {
+                slots: {
+                    default: '<jf-tab></jf-tab>'
+                }
             },
-            localVue,
-            mocks: {
-                $route,
-                $router,
-                JFrogEventBus
-            },
-            stubs: {
-                JfTab: '<div class="tab-data">Data for the tab</div>'
+            stubValue: {
+                stubs: {
+                    JfTab: '<div class="tab-data">Data for the tab</div>'
+                }
             }
-        });
+        })
         jest.runAllTimers();
         expect(wrapper.find('.tab-data').exists()).toBe(true);
     });
