1a7fd565d7c680234099b5b8a16fcf435d1403a7
dannyr <dannyr@jfrog.com>
2019-05-22 16:06:13 +0300

    jf-table-view/jf-table-view-wrapper - Virtual-Infinite scroll

--- src/components/JfTableViewComponent/index.vue
+++ src/components/JfTableViewComponent/index.vue
@@ -144,14 +144,13 @@
                 this.onUpdateFilter();
             },
             getActualPageHeight(maxForInfiniteVirtualScroll = false) {
-                if (this.options.rowsPerPage === 'auto' && $(this.$element).find('.table-rows-container').length) {
-                    if (maxForInfiniteVirtualScroll) {
-                        return $(this.$element).parent().height() - $(this.$element).find('.table-rows-container').position().top;
-                    }
-                    else {
-                        return Math.min($(this.$element).parent().height() - $(this.$element).find('.table-rows-container').position().top, parseFloat(this.options.rowHeight) * this.options.getPrePagedData().length);
-                    }
-                } else {
+                if (maxForInfiniteVirtualScroll) {
+                    return $(this.$element).parent().height() - $(this.$element).find('.table-rows-container').position().top;
+                }
+                else if (this.options.rowsPerPage === 'auto' && $(this.$element).find('.table-rows-container').length) {
+                    return Math.min($(this.$element).parent().height() - $(this.$element).find('.table-rows-container').position().top, parseFloat(this.options.rowHeight) * this.options.getPrePagedData().length);
+                }
+                else {
                     return parseFloat(this.options.rowHeight) * Math.min(this.options.rowsPerPage, this.options.getPrePagedData().length) + 2;
                 }
             },
