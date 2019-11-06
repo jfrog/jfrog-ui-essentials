c9d464defaa2906ec246c36731c042eb803d2901
Matan Gotlieb <matang@jfrog.com>
2019-11-06 21:25:06 +0200

    JFUIE-210 - Fix modal max-height calculation

--- src/ui_components/jfrog_modal/jfrog_modal.js
+++ src/ui_components/jfrog_modal/jfrog_modal.js
@@ -199,10 +199,12 @@ export class JFrogModal {
     }
 
     setModalBodyMaxHeight(modal,modalBody,modalHeight){
-        let headerHeight = modal.find('.modal-header').outerHeight() || 0,   // Header height
-            footerHeight = modal.find('.modal-footer').outerHeight() || 0,  // Footer height
-            maxHeight = modalHeight - headerHeight - footerHeight;
-        modalBody.css('max-height', maxHeight);
+        setTimeout(() => {
+            let headerHeight = modal.find('.modal-header').height() || 0,   // Header height
+                footerHeight = modal.find('.modal-footer').height() || 0,  // Footer height
+                maxHeight = modalHeight - headerHeight - footerHeight;
+            modalBody.css('max-height', maxHeight);
+        }, 50);
     }
 
 
